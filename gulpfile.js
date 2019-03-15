5947e12344675034aec99e5581db7d257bbd0ea7
vika <victorypa1992@yandex.ru>
2019-03-15 12:12:55 +0300

    header and about sec done

---
+++ gulpfile.js
@@ -0,0 +1,97 @@
+const   gulp          = require('gulp'),
+        gutil         = require('gulp-util' ),
+        sass          = require('gulp-sass'),
+        browsersync   = require('browser-sync'),
+        concat        = require('gulp-concat'),
+        uglify        = require('gulp-uglify'),
+        del           = require('del'),
+        cleancss      = require('gulp-clean-css'),
+        cache      	  = require('gulp-cache'),
+        rename        = require('gulp-rename'),
+        autoprefixer  = require('gulp-autoprefixer'),
+        imagemin      = require('gulp-imagemin'),
+        pngquant      = require('imagemin-pngquant'),
+        notify        = require('gulp-notify');
+
+gulp.task('browser-sync', () => {
+    browsersync({
+        server: {
+            baseDir: 'src'
+        },
+        notify: false,
+    })
+});
+
+gulp.task('sass', () => {
+	return gulp.src('src/sass/**/*.scss')
+	.pipe(sass({ outputStyle: 'expand' }).on("error", notify.onError()))
+	.pipe(autoprefixer(['last 15 versions']))
+	.pipe(gulp.dest('src/css'))
+	.pipe(browsersync.reload( {stream: true} ))
+});
+
+gulp.task('css-libs', ['sass'], () => {
+	return gulp.src([
+		'node_modules/normalize.css/normalize.css',
+		'node_modules/font-awesome/css/font-awesome.min.css',
+		'node_modules/bootstrap/dist/css/bootstrap.min.css'
+	])
+	.pipe(concat('libs.min.css'))
+	.pipe(cleancss( {level: { 1: { specialComments: 0 } } })) // Opt., comment out when debugging
+	.pipe(gulp.dest('src/css'))
+});
+
+gulp.task('fonts', () => {
+	return gulp.src('node_modules/font-awesome/fonts/*')
+		.pipe(gulp.dest('src/fonts'))
+});
+
+gulp.task('js', () => {
+	return gulp.src([
+		'node_modules/jquery/dist/jquery.min.js',
+		'src/js/common.js', // Always at the end
+		])
+	.pipe(concat('scripts.min.js'))
+	.pipe(uglify())
+	.pipe(gulp.dest('src/js'))
+	.pipe(browsersync.reload({ stream: true }))
+});
+
+gulp.task('img', () => {
+	return gulp.src('src/img/**/*') 
+			.pipe(cache(imagemin({
+					interlaced: true,
+					progressive: true,
+					svgoPlugins: [{removeViewBox: false}],
+					use: [pngquant()]
+			})))
+			.pipe(gulp.dest('dist/img')); 
+});
+
+gulp.task('clean', () => {
+	return del.sync('dist');
+});
+
+gulp.task('watch', ['js', 'browser-sync', 'css-libs'], () => {
+	gulp.watch('node_modules/font-awesome/fonts/*', ['fonts']);
+	gulp.watch('src/js/common.js', ['js']);
+	gulp.watch('src/sass/**/*.scss', ['sass']);
+	gulp.watch('src/*.html', browsersync.reload)
+});
+
+gulp.task('build', ['clean', 'img', 'sass', 'js'], () => {
+	const buildCss = gulp.src([
+		'src/css/libs.min.css',
+		'src/css/main.css'
+	])
+		.pipe(gulp.dest('dist/css'));
+
+	const buildFonts = gulp.src('src/fonts/**/*') 
+		.pipe(gulp.dest('dist/fonts'));
+
+	const buildJs = gulp.src('src/js/scripts.min.js') 
+		.pipe(gulp.dest('dist/js'));
+
+	const buildHtml = gulp.src('src/*.html')
+    .pipe(gulp.dest('dist'));
+});
