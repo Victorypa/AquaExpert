1b3fbfb04d2edb05788ab79bc0ef5730a68161df
vika <victorypa1992@yandex.ru>
2019-03-21 03:19:50 +0300

    add bubbles

--- src/js/common.js
+++ src/js/common.js
@@ -19,3 +19,18 @@ $(document).ready(function(){
 	});
 
 });
+
+//Randomly Generate Bubbles
+const container = document.querySelector('.circle')
+const emitterWidth = 100
+for (let i = 0; i < 5; i++) {
+  let bubble = document.createElement('div')
+  let {style} = bubble
+  let size = 10 + (Math.random() * 10) >>0
+  bubble.classList.add('bubble')
+  style.width = size + 'px'
+  style.height = size + 'px'
+  style.left = 10 + Math.random() * emitterWidth + 'px'
+  container.appendChild(bubble)
+  style.animationDelay = i + 0.5 + 's' 
+}
